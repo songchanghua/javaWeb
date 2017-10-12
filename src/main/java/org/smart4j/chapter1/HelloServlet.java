07998244b6941e055747b99707cbcaeef22258dd
songchanghua <1059863011@qq.com>
2017-10-12 09:14:26 +0800

    去掉***

--- src/main/java/org/smart4j/chapter1/HelloServlet.java
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -14,8 +14,6 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        System.out.println("************");
-        System.out.println("************");
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String currentTime = dateFormat.format(new Date());
         req.setAttribute("currentTime", currentTime);
